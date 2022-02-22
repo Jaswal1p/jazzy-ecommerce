3e067a37221acf1ce43ee82532f86c50623b7e2a
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-21 20:47:44 -0500

    added models for category & product

--- models/category.js
+++ models/category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // columns are defined as required by assignment
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
