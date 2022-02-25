422aab2e2a5510b69a15db816dd17d77e445d1bf
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-25 00:10:28 -0500

    code updated in multiple files

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,8 +17,35 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
+  console.log(req.params.id);
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name'],
+      },
+    //   { model: Tag, through: ProductTag,
+    //   attributes: ['tag_name'],
+    // }
+  ],
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+          return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
@@ -32,6 +59,7 @@ router.post('/', (req, res) => {
     }
   */
   Product.create(req.body)
+
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -64,6 +92,7 @@ router.put('/:id', (req, res) => {
     .then((product) => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
+
     })
     .then((productTags) => {
       // get list of current tag_ids
@@ -93,10 +122,27 @@ router.put('/:id', (req, res) => {
       // console.log(err);
       res.status(400).json(err);
     });
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Category.destroy({  
+    where: {    
+      id: req.params.id    
+    }  
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id '});
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
