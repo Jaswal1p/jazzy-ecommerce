1a38b28943b981c2169e67b73b47119db7c44a28
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-28 22:24:29 -0500

    corrected index.js in models to CASCADE

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 })
 // Categories have many Products
 Category.hasMany(Product, {
@@ -17,16 +18,18 @@ Product.belongsToMany(Tag, {
   through: {
   model:  ProductTag,
   unique: false
-  //foreignKey: 'product_id'
   },
+  foreignKey: 'product_id'
+  
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
   model: ProductTag,
   unique: false
-  //foreignKey: 'tag_id'
   },
+  foreignKey: 'tag_id'
+  
 })
 
 module.exports = {
